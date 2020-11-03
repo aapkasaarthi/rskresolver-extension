7618b50db0880db4d0681bbbdb1e9d629c9c285c
Anudit Nagar <nagaranudit@gmail.com>
2020-11-03 14:48:01 +0530

    Setup Chrome Extension

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rskresolver-extension
 RSK Testnet - RNS Resolver Chrome Extension
+
+![Extension Demo](extension-demo.gif)
+
+
---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.webRequest.onBeforeRequest.addListener((requestDetails)=>{
+    console.log(requestDetails);
+    let url = new URL(requestDetails.url);
+    fetch(`https://rskresolver.herokuapp.com/contenthash?domain=${url.hostname}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if (data.statusCode == '200'){
+            console.log('redirecting');
+            chrome.tabs.update(requestDetails.tabId, {url: data.response});
+        }
+    });
+
+},{urls: ["<all_urls>"], types: ['main_frame'], }, ['blocking']);
